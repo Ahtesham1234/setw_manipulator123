d56efe5eeaded1ba0ac661fa2e29f42cf115ad4d
Ahtesham Ul Haq(@hptouchpad519) <hptouchpad519@gmail.com>
2017-02-08 21:55:13 +0500

    Create width.cpp

---
+++ width.cpp
@@ -0,0 +1,15 @@
+//width1.cpp
+//demonstrates need for setw manipulator
+
+#include namespace std;  
+
+int main() 
+{
+long pop1= 2425785, pop2 47, pop3= 9761;
+
+cout<< "Location "<< "POP" <<endl
+<<"Portcity" << pop1 << endl
+<<"Hightown" <<pop2 << endl
+<<"Lowville" <<pop3 <<endl;
+return 0 ;
+}
